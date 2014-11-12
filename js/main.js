76efedb430eb9d0629b0a385c91ff3aaad883968
cook0246 <Devon@Devons-MacBook-Pro.local>
2014-11-11 21:41:18 -0500

    init and images

--- js/main.js
+++ js/main.js
@@ -3,5 +3,18 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init(){
 	//startup script
-	
+    $('.btn').mouseover(function(){
+        $(this).removeClass("inactive");
+        $(this).addClass("active");
+    });
+    $('.btn').mouseout(function(){
+        $(this).removeClass("active");
+        $(this).addClass("inactive");
+    });
+     $('#btnReset').click(function(){
+        alert("Values have been reset! :)");
+    });
+    
+
 }
+
